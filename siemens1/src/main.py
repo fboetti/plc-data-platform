a3cb6b6972f3b2b65d9a0ad04e1f4b6f4ad4c696
Fabio Leonardi <fabio.leonardi@ammagamma.com>
2024-04-12 08:52:07 +0200

    started siemens1 container

--- siemens1/src/main.py
+++ siemens1/src/main.py
@@ -1,4 +1,27 @@
-# --- SCRIPT BODY
+import time
+from snap7.server import Server as S7Server
+
+# -- This Package -- #
+from utils import (
+    DEFAULT_TCP_PORT,
+)
+
+
+def main():
+    print(f"Server 1 starting...")
+
+    # Creating a S7 instance
+    s7_server = S7Server()
+
+    # Start the server
+    s7_server.start(tcpport=DEFAULT_TCP_PORT)
+
+    while True:
+        event = s7_server.pick_event()
+        if event:
+            print(s7_server.event_text(event))
+        time.sleep(1)
+
 
 if __name__ == "__main__":
-    print("Hello Siemens!")
+    main()
---
+++ siemens1/src/utils/constants.py
@@ -0,0 +1,9 @@
+import typing
+
+__all__: typing.List[str] = [
+    "DEFAULT_TCP_PORT",
+    "DEFAULT_DB_SIZE",
+]
+
+DEFAULT_TCP_PORT: int = 102
+DEFAULT_DB_SIZE: int = 1024
